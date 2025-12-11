3f4cf83f1c5d789205bd646a14159369dcfc6cd4
Jack Pilowskuy <jackpilowsky@gmail.com>
2025-12-11 14:51:22 -0800

    Replace defaultdict with regular dict in task_5.py

--- task_5.py
+++ task_5.py
@@ -3,8 +3,6 @@ Task 5 - Hashing & Grouping
 Group anagrams together from a list of words.
 """
 
-from collections import defaultdict
-
 
 def group_anagrams(words: list[str]) -> list[list[str]]:
     """
@@ -17,11 +15,16 @@ def group_anagrams(words: list[str]) -> list[list[str]]:
         List of groups, where each group contains anagrams
     """
     # Use sorted characters as key to group anagrams
-    anagram_groups = defaultdict(list)
+    anagram_groups = {}
     
     for word in words:
         # Sort characters to create a key for anagrams
         key = "".join(sorted(word.lower()))
+        
+        # Check if key exists, create empty list if not
+        if key not in anagram_groups:
+            anagram_groups[key] = []
+        
         anagram_groups[key].append(word)
     
     # Return list of groups
